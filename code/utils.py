74bcad306351596b6d26441d4f31bb6707aaa866
lucapantea <luca26petru@gmail.com>
2023-06-11 11:51:02 +0200

    cuda deterministic flags

--- code/utils.py
+++ code/utils.py
@@ -104,6 +104,8 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
     torch.manual_seed(seed)
 
 def getFileName():
