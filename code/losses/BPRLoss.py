7196a4347d4550faaf7d3cc023d9e290cecc85c8
lucapantea <luca26petru@gmail.com>
2023-06-18 22:19:56 +0200

    added loss docstrings

--- code/losses/BPRLoss.py
+++ code/losses/BPRLoss.py
@@ -10,7 +10,26 @@ import torch
 
 
 class BPRLoss(object):
+    """
+    BPR loss function for pairwise ranking.
+
+    Args:
+        weight_decay (float): Weight decay coefficient.
+
+    Methods:
+        __call__(users_embeddings, pos_items_embeddings, neg_items_embeddings,
+                 users_embeddings_layer0, pos_items_embeddings_layer0, neg_items_embeddings_layer0):
+            Compute the BPR loss given the embeddings.
+
+    """
+
     def __init__(self, weight_decay):
+        """
+        Initialize the BPRLoss.
+
+        Args:
+            weight_decay (float): Weight decay coefficient.
+        """
         self.weight_decay = weight_decay
 
     def __call__(self,
@@ -20,7 +39,20 @@ class BPRLoss(object):
                  users_embeddings_layer0,
                  pos_items_embeddings_layer0,
                  neg_items_embeddings_layer0):
+        """
+        Compute the BPR loss given the embeddings.
 
+        Args:
+            users_embeddings: Embeddings of the users.
+            pos_items_embeddings: Embeddings of the positive items.
+            neg_items_embeddings: Embeddings of the negative items.
+            users_embeddings_layer0: Embeddings of the users in the first layer.
+            pos_items_embeddings_layer0: Embeddings of the positive items in the first layer.
+            neg_items_embeddings_layer0: Embeddings of the negative items in the first layer.
+
+        Returns:
+            torch.Tensor: Computed BPR loss.
+        """
         reg_loss = (1 / 2) * (users_embeddings_layer0.norm(2).pow(2) +
                               pos_items_embeddings_layer0.norm(2).pow(2) +
                               neg_items_embeddings_layer0.norm(2).pow(2)
