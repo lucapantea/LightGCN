e7f4728e0a925f7859abfed0a40622b69c9b4e85
lucapantea <luca26petru@gmail.com>
2023-06-23 15:44:06 +0200

    attention models

--- code/models/AttentionLightGCN.py
+++ code/models/AttentionLightGCN.py
@@ -1,3 +1,4 @@
+from code.datasets import BasicDataset
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,6 +6,113 @@ import math
 from .LightGCN import LightGCN
 from datasets import BasicDataset
 
+
+class BaseAttention(LightGCN):
+    """Extending LightGCN by adding a simple attention mechanism."""
+
+    def __init__(self, config: dict, dataset: BasicDataset):
+        super().__init__(config, dataset)
+        self.attention_weights = torch.nn.Parameter(torch.randn(config['lightGCN_n_layers'] + 1),
+                                                    requires_grad=True)
+        
+    def forward(self):
+        """Forward pass of the model."""
+        users_emb = self.embedding_user.weight
+        items_emb = self.embedding_item.weight
+        all_emb = torch.cat([users_emb, items_emb])
+        embs = [all_emb]
+
+        if self.config['dropout']:
+            if self.training:
+                print("dropping")
+                g_droped = self.__dropout(self.keep_prob)
+            else:
+                g_droped = self.graph
+        else:
+            g_droped = self.graph
+
+        for _ in range(self.n_layers):
+            if self.a_split:
+                temp_emb = []
+                for f in range(len(g_droped)):
+                    temp_emb.append(torch.sparse.mm(g_droped[f], all_emb))
+
+                side_emb = torch.cat(temp_emb, dim=0)
+                all_emb = side_emb
+            else:
+                all_emb = torch.sparse.mm(g_droped, all_emb)
+            embs.append(all_emb)
+        embs = torch.stack(embs, dim=1)
+
+        if self.config['save_embs']:
+            self.embs = embs
+
+        # Attention Mechanism
+        attention_scores = F.softmax(self.attention_weights, dim=0)
+        light_out = torch.sum(embs * attention_scores.view(1, -1, 1), dim=1)
+
+        all_users_embeddings, all_items_embeddings = torch.split(
+            light_out, [self.num_users, self.num_items])
+
+        return all_users_embeddings, all_items_embeddings
+    
+class FinerAttention(LightGCN):
+    """Extending LightGCN by adding a finer attention mechanism."""
+    def __init__(self, config: dict, dataset: BasicDataset):
+        super().__init__(config, dataset)
+        self.attention_weights_users = torch.nn.Parameter(torch.randn(config['lightGCN_n_layers'] + 1),
+                                                          requires_grad=True)
+        self.attention_weights_items = torch.nn.Parameter(torch.randn(config['lightGCN_n_layers'] + 1),
+                                                          requires_grad=True)
+        
+    def forward(self):
+        """Forward pass of the model."""
+        users_emb = self.embedding_user.weight
+        items_emb = self.embedding_item.weight
+        all_emb = torch.cat([users_emb, items_emb])
+        embs = [all_emb]
+
+        if self.config['dropout']:
+            if self.training:
+                print("dropping")
+                g_droped = self.__dropout(self.keep_prob)
+            else:
+                g_droped = self.graph
+        else:
+            g_droped = self.graph
+
+        for _ in range(self.n_layers):
+            if self.a_split:
+                temp_emb = []
+                for f in range(len(g_droped)):
+                    temp_emb.append(torch.sparse.mm(g_droped[f], all_emb))
+
+                side_emb = torch.cat(temp_emb, dim=0)
+                all_emb = side_emb
+            else:
+                all_emb = torch.sparse.mm(g_droped, all_emb)
+            embs.append(all_emb)
+        embs = torch.stack(embs, dim=1)
+
+        if self.config['save_embs']:
+            self.embs = embs
+
+        # Calculate the softmax
+        attention_scores_users = F.softmax(self.attention_weights_users, dim=0).view(1, -1, 1)
+        attention_scores_items = F.softmax(self.attention_weights_items, dim=0).view(1, -1, 1)
+
+        # Create block diagonal attention matrix
+        attention_scores = torch.cat([attention_scores_users.repeat(self.num_users, 1, 1),
+                                      attention_scores_items.repeat(self.num_items, 1, 1)], dim=0)
+
+        # Compute weighted sum in one step
+        light_out = torch.sum(embs * attention_scores, dim=1)
+
+        all_users_embeddings, all_items_embeddings = torch.split(
+            light_out, [self.num_users, self.num_items])
+
+        return all_users_embeddings, all_items_embeddings
+        
 
 class ScaledDotProductAttentionLightGCN(LightGCN):
     """Extending LightGCN by adding scaled dot-product attention."""
